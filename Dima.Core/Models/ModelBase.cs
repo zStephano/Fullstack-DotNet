84e87a30cd5fcaec02f8dd8944be37bab813ba1f
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-08-17 22:55:46 -0300

    Project creation and little components implementation

---
+++ Dima.Core/Models/ModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public abstract class ModelBase
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt {  get; set; }
+    }
+}
---
+++ Dima.Core/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using Dima.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Models
+{
+    public class Transaction : ModelBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public DateTime? PaidOrReceivedAt { get; set; }
+        public ETransactionType Type { get; set; } = ETransactionType.Withdraw;
+        public decimal Amount { get; set; }
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
