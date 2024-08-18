d635165075e973b0371704834682f6011d1c6b23
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-08-18 19:58:49 -0300

    feat: add genericals to requests and responses + pagination

---
+++ Dima.Core/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core
+{
+    public static class Configuration
+    {
+        public const int DefaultStatusCode = 200;
+
+        public const int DefaultPageNumber = 1;
+        public const int DefaultPageSize = 25;
+    }
+}
---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dima.Core.Requests
+{
+    public abstract class PagedRequest : RequestBase
+    {
+        public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
+        public int PageSize { get; set; } = Configuration.DefaultPageSize;
+    }
+}
