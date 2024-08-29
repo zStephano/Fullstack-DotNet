42b6d7b664967157c94e6872da1e78e87dccdc21
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-08-28 22:45:46 -0300

    Add Identity to project
    Add:
    - Identity;
    - Tables for users and roles;
    - Services and App references to Authentication & Authorization;

---
+++ Dima.Api/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Dima.Api.Models
+{
+    public class User : IdentityUser<long>
+    {
+        public List<IdentityRole<long>>? Roles { get; set; }
+    }
+}
--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,10 +1,23 @@
 using Dima.Api.Data;
 using Dima.Api.Endpoints;
 using Dima.Api.Handlers;
+using Dima.Api.Models;
 using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName); // Full Qualified Name
+});
+
+builder.Services
+    .AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
 
 var connectionString = builder
     .Configuration
@@ -15,15 +28,18 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     x.UseSqlServer(connectionString);
 });
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x =>
-{
-    x.CustomSchemaIds(n => n.FullName); // Full Qualified Name
-});
+builder.Services
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole<long>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseSwagger();
 app.UseSwaggerUI();
