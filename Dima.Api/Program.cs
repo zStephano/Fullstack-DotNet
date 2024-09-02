c35cbc0339f29d1a8dc7c3c85f17d39e24388201
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-09-02 20:42:25 -0300

    Finalization of Identity part, add extension methods for Builder and App

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,50 +1,23 @@
-using Dima.Api.Data;
+using Dima.Api;
+using Dima.Api.Common.Api;
 using Dima.Api.Endpoints;
-using Dima.Api.Handlers;
-using Dima.Api.Models;
-using Dima.Core.Handlers;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Dima.Core;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x =>
-{
-    x.CustomSchemaIds(n => n.FullName); // Full Qualified Name
-});
-
-builder.Services
-    .AddAuthentication(IdentityConstants.ApplicationScheme)
-    .AddIdentityCookies();
-builder.Services.AddAuthorization();
-
-var connectionString = builder
-    .Configuration
-    .GetConnectionString("DefaultConnection") ?? string.Empty;
-
-builder.Services.AddDbContext<AppDbContext>(x =>
-{
-    x.UseSqlServer(connectionString);
-});
-
-builder.Services
-    .AddIdentityCore<User>()
-    .AddRoles<IdentityRole<long>>()
-    .AddEntityFrameworkStores<AppDbContext>()
-    .AddApiEndpoints();
-
-builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+builder.AddConfiguration();
+builder.AddSecurity();
+builder.AddDataContexts();
+builder.AddCrossOrigin();
+builder.AddDocumentation();
+builder.AddServices();
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+    app.ConfigureDevEnvironment();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.MapGet("/", () => new { message = "OK!" });
+app.UseCors(ApiConfiguration.CorsPolicyName);
+app.UseSecurity();
 app.MapEndpoints();
 
 app.Run();
--- Dima.Core/Configuration.cs
+++ Dima.Core/Configuration.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace Dima.Core;
 
-namespace Dima.Core
+public static class Configuration
 {
-    public static class Configuration
-    {
-        public const int DefaultStatusCode = 200;
+    public const int DefaultStatusCode = 200;
+    public const int DefaultPageNumber = 1;
+    public const int DefaultPageSize = 25;
 
-        public const int DefaultPageNumber = 1;
-        public const int DefaultPageSize = 25;
-    }
+    public static string ConnectionString { get; set; } = string.Empty;
+    public static string BackendUrl { get; set; } = string.Empty;
+    public static string FrontendUrl { get; set; } = string.Empty;
+
+    public static long PremiumPrice { get; set; } = 79990;
 }
