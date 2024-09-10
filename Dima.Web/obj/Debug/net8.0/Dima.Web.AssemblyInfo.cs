efdbc8191be3ac04cf4afb5683e11f9c84ade42a
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-09-10 15:55:39 -0300

    Finalization the creation of login and register view and components

--- Dima.Web/obj/Debug/net8.0/Dima.Web.AssemblyInfo.cs
+++ Dima.Web/obj/Debug/net8.0/Dima.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a419bc8c8ea2d3095f36c1f03e0fe84da5e658b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9169c1cdf4315b8c2ffe77448f2ecb5b744d742d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
