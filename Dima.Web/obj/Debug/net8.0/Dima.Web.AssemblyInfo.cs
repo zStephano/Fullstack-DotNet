aec834990777091b7c58c4d0768d38043b96cf12
Gustavo Stephano <38302193+zStephano@users.noreply.github.com>
2024-09-16 16:44:38 -0300

    Adding dark/light mode and System Preferences automation

--- Dima.Web/obj/Debug/net8.0/Dima.Web.AssemblyInfo.cs
+++ Dima.Web/obj/Debug/net8.0/Dima.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efdbc8191be3ac04cf4afb5683e11f9c84ade42a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea86fdaae82cb3a1e80ce8dfe1aa6ead8053c5a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
